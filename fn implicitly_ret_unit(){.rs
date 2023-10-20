7fb3763f7ce517c28250c8be3d0b18b0f9e4e251
kartikeyssj <kartikey492.be22@chitkara.edu.in>
2023-10-20 14:22:41 +0530

    new programs added

---
+++ fn implicitly_ret_unit(){.rs
@@ -0,0 +1,12 @@
+fn implicitly_ret_unit(){
+    println!("I will return a ()");
+}
+
+fn main(){
+    let _v: () = ();
+
+    let v: (i32 , i32) = (2 , 3);
+    assert_eq!(_v , implicitly_ret_unit());
+
+    println!("Success!");
+}
